6452d887f13356b375a2ce6e285e757990d430a7
KennethLloyd <verastiguelloyd@gmail.com>
2020-12-18 08:52:53 +0800

    Add transaction validator

---
+++ src/validators/transaction.js
@@ -0,0 +1,49 @@
+const JoiBase = require('joi');
+const JoiDate = require('@hapi/joi-date');
+
+const Joi = JoiBase.extend(JoiDate);
+
+const addTransaction = async (req, res, next) => {
+  const bodySchema = Joi.object({
+    transactionDate: Joi.date().format('YYYY-MM-DD HH:mm').required(),
+    transactionType: Joi.string().valid('Income', 'Expense').required(),
+    name: Joi.string().required(),
+    amount: Joi.number().required(),
+    description: Joi.string().empty(''),
+  });
+
+  try {
+    const body = await bodySchema.validateAsync(req.body);
+    req.body = body;
+    return next();
+  } catch (err) {
+    return res.status(400).send({
+      error: err.details[0].message,
+    });
+  }
+};
+
+const getAllTransactions = async (req, res, next) => {
+  const querySchema = Joi.object({
+    sortBy: Joi.string().empty(''),
+    sortOrder: Joi.string().uppercase().valid('ASC', 'DESC').empty(''),
+    from: Joi.date().format('YYYY-MM-DD').empty(''),
+    to: Joi.date().format('YYYY-MM-DD').empty(''),
+  });
+
+  try {
+    const query = await querySchema.validateAsync(req.query);
+    req.query = query;
+
+    return next();
+  } catch (err) {
+    return res.status(400).send({
+      error: err.details[0].message,
+    });
+  }
+};
+
+module.exports = {
+  addTransaction,
+  getAllTransactions,
+};
